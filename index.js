70e666208d59aa72d6241df67aa54ef0e7a2bd1b
slava hatnuke <slava.hatnuke@gmail.com>
2017-04-05 22:55:15 +0300

    wip

--- index.js
+++ index.js
@@ -1,1 +1,129 @@
-module.exports = {}
+class Value {
+    constructor(value = undefined) {
+        this.value = value;
+    }
+
+    get() {
+        return this.value;
+    }
+
+    set(value) {
+        this.value = value;
+    }
+}
+
+class Copier {
+    constructor(depth = null, options = {}) {
+        this.options = Object.assign({strict: true}, options);
+        this.depth = depth;
+    }
+
+    copy(object) {
+        return copy(object, this.depth, this.options);
+    }
+
+    isSame(object1, object2) {
+        return isSame(object1, object2, this.depth, this.options);
+    }
+}
+
+const copier = (depth = null, options = {}) => new Copier(depth, options);
+
+const isObject = (object) => object instanceof Object;
+const isFunction = (object) => object instanceof Function;
+const isArray = (object) => Array.isArray(object);
+//TODO @@@slava do I need?
+const isPureObject = (object) => isObject(object) && !isFunction(object) && !isArray(object);
+
+
+const field = (object, name, getter = () => undefined) => {
+    if (isObject(object) && !object[name]) {
+        Object.defineProperty(object, name, {
+            get: getter,
+            enumerable: false,
+            configurable: false
+        });
+    }
+
+    return object;
+};
+
+const originSymbol = '__symbol_icopier_origin';
+const getOriginName = (name) => `${originSymbol}:${name}`;
+
+const setOrigin = (object, name, value) => {
+    if (isObject(object) && !object[getOriginName(name)]) {
+        const _value = new Value(value);
+        field(object, getOriginName(name), () => _value)
+    }
+};
+
+const getOrigin = (object, name) => {
+    if (isObject(object)) {
+        if (object[getOriginName(name)]) {
+            return object[getOriginName(name)].get();
+        } else {
+            return object[name];
+        }
+    } else {
+        return undefined;
+    }
+};
+
+
+const _copy = (object, depth, options = {}, level = 0) => {
+    if (depth !== null && level > depth) {
+        return object;
+    }
+
+    if (isFunction(object)) {
+        return object;
+    }
+
+    if (!isObject(object)) {
+        return object;
+    }
+
+    let names = [];
+    let result = null;
+
+    if (isArray(object)) {
+        names = [...Array(object.length).keys()];
+        result = [];
+    } else {
+        names = Object.getOwnPropertyNames(object);
+        const Clone = function () {
+        };
+
+        Clone.prototype = object;
+        result = new Clone();
+    }
+
+    for (let i = 0; i < names.length; i++) {
+        let name = names[i];
+
+        if (isObject(object[name])) {
+            //TODO @@@slava strict
+            setOrigin(result, name, object[name]);
+            result[name] = _copy(object[name], depth, options, level + 1);
+        } else {
+            result[name] = object[name];
+        }
+    }
+
+    return result;
+};
+
+const copy = (object, depth = null, options = {}) => {
+    return _copy(object, depth, Object.assign({strict: true}, options))
+};
+
+const isSame = (object1, object2, depth = null, options = {}) => {
+
+};
+
+module.exports = {
+    copy,
+    isSame,
+    copier
+};
--- test/test.js
+++ test/test.js
@@ -1,8 +1,173 @@
 const assert = require('assert');
 
-describe('test', () => {
-    it('test', () => Promise.resolve().then(() => {
-        assert.equal(1, 1);
-        // assert.equal('actual', 'expected')
-    }))
+const {copy, isSame} = require('..');
+
+describe('iCopier', () => {
+    it('example (this object is flat)', () => {
+        let user = {
+            name: 'slava'
+        };
+
+        let clone = copy(user);
+
+        let fields = Object.keys(clone);
+        assert.deepEqual(fields, ['name']);
+        assert(clone !== user);
+
+        assert.deepEqual(clone, {
+            name: 'slava'
+        });
+
+        assert.equal(JSON.stringify(clone), '{"name":"slava"}');
+
+        class User {
+            constructor(name) {
+                this.name = name;
+            }
+        }
+
+        user = new User('slava');
+        clone = copy(user);
+
+        assert(clone instanceof User);
+        assert(clone !== user);
+
+        assert.deepEqual(clone, {
+            name: 'slava'
+        });
+    });
+
+    it('deep example (infinity depth)', () => {
+        let user = {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        };
+
+        let clone = copy(user);
+
+        let fields = Object.keys(clone);
+        assert.deepEqual(fields, ['name', 'settings', 'picked']);
+
+        assert.deepEqual(clone, {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        });
+
+        assert.equal(JSON.stringify(clone), '{"name":"slava","settings":{"allow_notification":true},"picked":[1,2,5,8]}');
+
+        user.settings.allow_notification = true;
+        user.picked.splice(0);
+
+        assert.deepEqual(user, {
+            "name": "slava",
+            "settings": {"allow_notification": true},
+            "picked": []
+        });
+
+        assert.deepEqual(clone, {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        });
+    });
+
+    it('deep example (depth=0)', () => {
+        let user = {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        };
+
+        let clone = copy(user, 0);
+
+        assert.deepEqual(clone, {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        });
+
+        user.name = 'name-updated';
+
+        assert.deepEqual(clone, {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        });
+
+        assert.deepEqual(user, {
+            name: 'name-updated',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        });
+
+        user.settings.allow_notification = false;
+        user.picked.splice(0);
+
+        assert.deepEqual(user, {
+            name: 'name-updated',
+            settings: {
+                allow_notification: false,
+            },
+            picked: []
+        });
+
+        // it's just cloned the zero (0) level of model but settings and picked still a ref
+        assert.deepEqual(clone, {
+            name: 'slava',
+            settings: {
+                allow_notification: false,
+            },
+            picked: []
+        });
+
+    })
+
+    it('deep example (depth=1)', () => {
+        let user = {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1, 2, 5, 8]
+        };
+
+        let clone = copy(user, 1);
+        user.name = 'Slava (updated)';
+        user.settings.allow_notification = false;
+        user.picked.splice(0);
+
+        assert.deepEqual(user, {
+            name: 'Slava (updated)',
+            settings: {
+                allow_notification: false,
+            },
+            picked: []
+        });
+
+        // it's cloned the 1st level of model but settings and picked still a ref
+        assert.deepEqual(clone, {
+            name: 'slava',
+            settings: {
+                allow_notification: true,
+            },
+            picked: [1,2,5,8]
+        });
+
+    })
 });
